0f8a98f34d11b39e32eb72f9220476dd2bec7ce2
priya4035 <75771900+priya4035@users.noreply.github.com>
2020-12-19 09:31:07 +0530

    initilization example

--- Bike.java
+++ Bike.java
@@ -1,33 +1,35 @@
 
 public class Bike {
+ 
+	String name;
 	String color;
-	String brand;
-	double cost;
-	String typeofbike;
-
-	Bike() {
-
+	double price;
+	 
+	{
+		System.out.println("initlization 1");
 	}
-
-	Bike(String inColor, String inBrand) {
-		color = inColor;
-		brand = inBrand;
-
+	 
+	Bike(){
+		System.out.println("no arg const");
 	}
-
-	Bike(String inColor, String inBrand, double inCost, String inTypeofbike) {
-		color = inColor;
-		brand = inBrand;
-		cost = inCost;
-		typeofbike = inTypeofbike;
-
+	{
+		System.out.println("initilization 2");
 	}
-
+	Bike(String inName,String inColor,double inPrice){
+		name=inName;
+		color=inColor;
+		price=inPrice;
+	}
 	void driving() {
-		System.out.println("bike driving");
+		System.out.println("driving the bike");
 	}
-
-	void racing() {
-		System.out.println("bike racing");
+	void displayDetails() {
+		System.out.println("NAME: "+name+" "+ "COLOR: "+color +" "+ "PRICE: "+price);
+	}
+	{
+		System.out.println("initilization 3");
+	}
+	static {
+		System.out.println("initilization 4");
 	}
 }
--- BikeTester.java
+++ BikeTester.java
@@ -2,34 +2,16 @@
 public class BikeTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Bike activaHonda = new Bike();
-		System.out.println("bike color :" + activaHonda.color);
-		System.out.println("bike brand :" + activaHonda.brand);
-		System.out.println("bike cost :" + activaHonda.cost);
-		System.out.println("is it petrol or electric :" + activaHonda.typeofbike);
-
-		System.out.println("////////////");
-		Bike dio = new Bike("Black", "Dio", 60000, "Petrol");
-
-		System.out.println("bike color :" + dio.color);
-		System.out.println("bike brand :" + dio.brand);
-		System.out.println("bike cost :" + dio.cost);
-		System.out.println("is it petrol or electric :" + dio.typeofbike);
 		
-		System.out.println("===============");
-		 
-		Bike ktm = new Bike("White","KTM");
-		System.out.println("bike color :" + ktm.color);
-		System.out.println("bike brand :" + ktm.brand);
-              
-		System.out.println(":::::::::::::::::::::");
-	 dio.color ="red";
-	 System.out.println("color : "+ dio.color);
-	 
-	  System.out.println(",,,,,,,,");
-	 dio.driving();
-	  activaHonda.racing();
+		Bike rx= new Bike("RX","Red",85000.00);
+		rx.displayDetails();
+		rx.driving();
+
+		 System.out.println("////////////////");
+		  
+		 Bike ktm = new Bike("KTM","Orange",100000.00);
+		 ktm.driving();
+		 ktm.displayDetails();
 	}
 
 }
